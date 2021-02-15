ccb80b25c7ec8e9bd56eb916ab0a6f2d23f0bbdc
kirill-biotech <krl_msv@hotmail.com>
2021-02-15 10:19:21 -0800

    Mid class save

--- Week_4/Week_4_Mon.R
+++ Week_4/Week_4_Mon.R
@@ -18,10 +18,26 @@ filter(.data = penguins, species == "Adelie"|"Gentoo")
 data2<-mutate(.data = penguins, 
               body_mass_kg = body_mass_g/1000
               ) #mass in g to kg
+View(data2)
 
-data2<-mutate(.data = penguins, 
+data3<-mutate(.data = penguins, 
               body_mass_kg = body_mass_g/1000,
               bill_ratio = bill_length_mm/bill_depth_mm
               ) #calc bill length to depth ratios in new column
+View(data3)
 
-View(data2)
+data4<- mutate(.data = penguins,
+               after_2008 = ifelse( year>2008, "After 2008", "Before 2008")
+               ) #assign After 2008 or Before 2008 in new column
+View(data4)
+
+data5<- mutate(.data = penguins,
+               fl_lgth_and_mass = flipper_length_mm+body_mass_g
+               )
+View(data5)
+
+data6<- mutate(.data = penguins,
+               sex_to_caps = ifelse(sex == "male", "Male","Female")
+               )
+View(data6)
+
