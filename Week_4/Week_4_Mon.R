5d5888ffd1e714ffebfa9b3a386064fbf0c270db
kirill-biotech <krl_msv@hotmail.com>
2021-02-15 09:49:12 -0800

    Mid class save

--- Week_4/Week_4_Mon.R
+++ Week_4/Week_4_Mon.R
@@ -7,3 +7,21 @@ library(here)
 
 ##### Load Data #####
 glimpse(penguins)
+
+##### Data Manipulation #####
+### Filter practice
+filter(.data = penguins, year == 2008|2009 )
+filter(.data = penguins, island !="Dream" )
+filter(.data = penguins, species == "Adelie"|"Gentoo")
+
+### Mutate Practice
+data2<-mutate(.data = penguins, 
+              body_mass_kg = body_mass_g/1000
+              ) #mass in g to kg
+
+data2<-mutate(.data = penguins, 
+              body_mass_kg = body_mass_g/1000,
+              bill_ratio = bill_length_mm/bill_depth_mm
+              ) #calc bill length to depth ratios in new column
+
+View(data2)
