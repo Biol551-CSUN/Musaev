0965b3af41db8be703548bd0440d3367e92bc84c
kirill-biotech <krl_msv@hotmail.com>
2021-02-15 11:18:46 -0800

    Group work part 1 done

---
+++ Week_4/Scripts/Week_4_Mon_Group_work.R
@@ -0,0 +1,19 @@
+##### Feb 15, 2021 #####
+##### Group work #####
+
+##### Load Libraries #####
+library(palmerpenguins)
+library(tidyverse)
+library(here)
+
+##### Load Data #####
+glimpse(penguins)
+
+##### Analysis #####
+mean_and_var <- penguins %>%
+  group_by(species, island, sex) %>%
+  drop_na(sex, body_mass_g, island, species) %>%
+  summarise (mean_mass = mean(body_mass_g, na.rm = TRUE),
+             var_mass = var(body_mass_g, na.rm = TRUE)
+             )
+view(mean_and_var)
