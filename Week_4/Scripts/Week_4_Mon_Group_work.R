a22c4134f24c8d476c53ce8dbcf57c1b9b2deb48
kirill-biotech <krl_msv@hotmail.com>
2021-02-15 12:01:34 -0800

    Group assignment done

--- Week_4/Scripts/Week_4_Mon_Group_work.R
+++ Week_4/Scripts/Week_4_Mon_Group_work.R
@@ -11,6 +11,7 @@ glimpse(penguins)
 
 ##### Analysis #####
 ### Part 1 ###
+
 mean_and_var <- penguins %>%
   group_by(species, island, sex) %>%
   drop_na(sex, body_mass_g, island, species) %>%
@@ -18,3 +19,31 @@ mean_and_var <- penguins %>%
              var_mass = var(body_mass_g, na.rm = TRUE)
              )
 view(mean_and_var)
+
+### Part 2 ###
+penguins %>%
+  filter (sex == "female") %>%
+  mutate(log_mass = log(body_mass_g)) %>%
+  select(species, island, sex, log_mass)%>%
+  ggplot(aes(x = species, 
+             y = log_mass)
+  ) + 
+  geom_violin() +
+  geom_jitter(
+    aes(x = species,
+        y = log_mass,
+        color = island),
+    position = position_jitterdodge(dodge = 0.1)
+  ) +
+  labs(title = "Penguin log of Body Mass by Species", #custom labels
+       subtitle = "On Biscoe, Dream and Torgersen Islands",
+       caption = "Source: Palmer Station LTER / palmerpenguins package",
+       x = "Species",
+       y = "log of Body Mass, grams",
+       color = "Island"
+  )+
+  scale_color_manual(labels = c("Biscoe","Dream","Torgersen"), #custom colors
+                     values = c("red3","mediumblue","limegreen")
+  )+
+  theme_bw()
+
