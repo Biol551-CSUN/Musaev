8b39dee8120cc1531978e3e62a9d182dbc177dd7
kirill-biotech <krl_msv@hotmail.com>
2021-02-17 12:11:07 -0800

    Wednesday group work submission

--- Week_4/Scripts/Week_4_Wed_Group_Work.R
+++ Week_4/Scripts/Week_4_Wed_Group_Work.R
@@ -6,6 +6,19 @@ library(tidyverse)
 library(here)
 
 ##### Load Data #####
-ChemData<-read_csv(here("Week_4","data", "chemicaldata_maunalua.csv"))
-View(ChemData)
-glimpse(ChemData)
+ChemData<-read_csv(here("Week_4","data", "chemicaldata_maunalua.csv")) #path to data file
+glimpse(ChemData) #verify data is loaded
+#View(ChemData) #view full data if necessary
+
+##### Script #####
+ChemData_clean<-ChemData %>% #create pipeline for cleaning and formatting
+  filter(complete.cases(.)) %>% #remove rows with NAs to clean data
+  separate(col = Tide_time, #select column to split
+         into = c("Tide","Time"), #split into new columns Tide and Time
+         sep = "_", #separate values by underscore
+         remove = FALSE #keep original column
+)
+#view(ChemData_clean) #view full clean data in necessary
+
+
+
