3e6ca668a248661c08c134dc694d6bbe055d62dd
kirill-biotech <krl_msv@hotmail.com>
2021-02-17 11:06:56 -0800

    Group work started

---
+++ Week_4/Scripts/Week_4_Wed_Group_Work.R
@@ -0,0 +1,11 @@
+##### Week 4, Wed Group Work #####
+##### Feb 17, 2021 #####
+
+##### Load Libraries #####
+library(tidyverse)
+library(here)
+
+##### Load Data #####
+ChemData<-read_csv(here("Week_4","data", "chemicaldata_maunalua.csv"))
+View(ChemData)
+glimpse(ChemData)
