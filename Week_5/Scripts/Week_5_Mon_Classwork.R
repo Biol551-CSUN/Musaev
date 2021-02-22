aea966cb1af098329320e0b9bdb531a41e2d21bb
kirill-biotech <krl_msv@hotmail.com>
2021-02-22 10:33:16 -0800

    Classwork started

--- Week_5/Scripts/Week_5_Mon_Classwork.R
+++ Week_5/Scripts/Week_5_Mon_Classwork.R
@@ -1,6 +1,22 @@
 ##### Feb 22, 2021 #####
 ##### Classwork #####
 
+
 ##### Load Libraries #####
 library(tidyverse)
 library(here)
+
+
+##### Load Data #####
+# Environmental data from each site
+EnviroData<-read_csv(here("Week_5","data", "site.characteristics.data.csv"))
+
+#Thermal performance data
+TPCData<-read_csv(here("Week_5","data","Topt_data.csv"))
+
+### Check Data ###
+glimpse(EnviroData)
+#view(EnviroData) 
+
+glimpse(TPCData)
+#view(TPCData)
--- Week_5/Week_5_Mon_Classwork.R
+++
@@ -1,6 +0,0 @@
-##### Feb 22, 2021 #####
-##### Classwork #####
-
-##### Load Libraries #####
-library(tidyverse)
-library(here)
