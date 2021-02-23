613581d481bd857807a0dd871ab12162ff68df10
kirill-biotech <krl_msv@hotmail.com>
2021-02-23 15:25:43 -0800

    Classwork continued

--- Week_5/Scripts/Week_5_Mon_Classwork.R
+++ Week_5/Scripts/Week_5_Mon_Classwork.R
@@ -15,8 +15,42 @@ EnviroData<-read_csv(here("Week_5","data", "site.characteristics.data.csv"))
 TPCData<-read_csv(here("Week_5","data","Topt_data.csv"))
 
 ### Check Data ###
-glimpse(EnviroData)
-#view(EnviroData) 
+#glimpse(EnviroData)
+#view(EnviroData) #open data in a tab to review its structure
+#glimpse(TPCData)
+#view(TPCData) #open data in a tab to review its structure
 
-glimpse(TPCData)
-#view(TPCData)
+
+##### Script #####
+### Data formatting ###
+EnviroData_wide <- EnviroData %>% #start pipeline for pivoting
+  pivot_wider(names_from = parameter.measured, #put types of parameters measured in the columns...
+              values_from = values #...and populate rows with "values"
+              ) %>% #pass pipeline once more to arrange command on the next line
+arrange(site.letter) #arrange by site identifier
+#View(EnviroData_wide) #verify pivoting and arranging
+
+FullData_left<- left_join(TPCData, EnviroData_wide) #merge EnviroData_wide into TPCData, omitting sites absent in EnviroData_wide
+#head(FullData_left) #demonstrate the merging
+
+FullData_left<- left_join(TPCData, EnviroData_wide) %>% #pipeline to relocate
+  relocate(where(is.numeric), .after = where(is.character)) #move columns with strings to the left, numeric data to the right
+#head(FullData_left)#demonstrate relocation
+
+### Statistics ###
+#view(FullData_left) #open new tab with data to be processed
+FullData_long <- FullData_left %>% #create pipeline for pivoting
+  pivot_longer(cols = E:Topt, #select all numeric data columns (light through substrate.cover)
+               names_to = "Variables", #create column for each variable type
+               values_to = "Values" #populate column withreadings for each data type
+  )
+#view(FullData_long) #verify proper pivoting
+
+FullData_stats <- FullData_long %>% #create pipeline for stats calcs
+  group_by (Variables, site.letter) %>% #select parameters to group by
+  summarize(site_mean = mean(Values, na.rm = TRUE), #calc means in new column site_mean
+            site_var = var(Values, na.rm = TRUE) #calc variances in new column site_var
+            )
+view(FullData_stats)
+  
+  
