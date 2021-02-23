08c1009dd8ade5471f263aa4ea02f4e3e14961bc
kirill-biotech <krl_msv@hotmail.com>
2021-02-23 15:25:43 -0800

    Classwork continued

--- Week_5/Scripts/Week_5_Mon_Classwork.R
+++ Week_5/Scripts/Week_5_Mon_Classwork.R
@@ -16,7 +16,32 @@ TPCData<-read_csv(here("Week_5","data","Topt_data.csv"))
 
 ### Check Data ###
 glimpse(EnviroData)
-#view(EnviroData) 
-
+#view(EnviroData) #open data in a tab to review its structure
 glimpse(TPCData)
-#view(TPCData)
+#view(TPCData) #open data in a tab to review its structure
+
+
+##### Script #####
+### Data formatting ###
+EnviroData_wide <- EnviroData %>% #start pipeline for pivoting
+  pivot_wider(names_from = parameter.measured, #put types of parameters measured in the columns...
+              values_from = values #...and populate rows with "values"
+              )
+arrange(site.letter) #arrange by site identifier
+View(EnviroData_wide) #verify pivoting and arranging
+
+FullData_left<- left_join(TPCData, EnviroData_wide) #merge EnviroData_wide into TPCData, omitting sites absent in EnviroData_wide
+#head(FullData_left) #demonstrate the merging
+
+FullData_left<- left_join(TPCData, EnviroData_wide) %>% #pipeline to relocate
+  relocate(where(is.numeric), .after = where(is.character)) #move columns with strings to the left, numeric data to the right
+head(FullData_left)#demonstrate relocation
+
+### Statistics ###
+view(FullData_left) #open new tab with data to be processed
+FullData_long <- FullData_left %>% #create pipeline for pivoting
+  pivot_longer(cols = light:substrate.cover, #select all numeric data columns (light through substrate.cover)
+               names_to = "Variables", #create column for each variable type
+               values_to = "Values" #populate column withreadings for each data type
+  )
+view(FullData_long)
