c12164bfb28fc3f93f7255af09d48a4f61501490
kirill-biotech <krl_msv@hotmail.com>
2021-02-23 16:44:28 -0800

    Classwork upload error resolution

--- Week_5/Scripts/Week_5_Mon_Classwork.R
+++ Week_5/Scripts/Week_5_Mon_Classwork.R
@@ -15,15 +15,9 @@ EnviroData<-read_csv(here("Week_5","data", "site.characteristics.data.csv"))
 TPCData<-read_csv(here("Week_5","data","Topt_data.csv"))
 
 ### Check Data ###
-<<<<<<< HEAD
 #glimpse(EnviroData)
 #view(EnviroData) #open data in a tab to review its structure
 #glimpse(TPCData)
-=======
-glimpse(EnviroData)
-#view(EnviroData) #open data in a tab to review its structure
-glimpse(TPCData)
->>>>>>> 08c1009dd8ade5471f263aa4ea02f4e3e14961bc
 #view(TPCData) #open data in a tab to review its structure
 
 
@@ -32,22 +26,15 @@ glimpse(TPCData)
 EnviroData_wide <- EnviroData %>% #start pipeline for pivoting
   pivot_wider(names_from = parameter.measured, #put types of parameters measured in the columns...
               values_from = values #...and populate rows with "values"
-<<<<<<< HEAD
-              ) %>% #pass pipeline once more to arrange command on the next line
-arrange(site.letter) #arrange by site identifier
+  ) %>% #pass pipeline once more to arrange command on the next line
+  arrange(site.letter) #arrange by site identifier
 #View(EnviroData_wide) #verify pivoting and arranging
-=======
-              )
-arrange(site.letter) #arrange by site identifier
-View(EnviroData_wide) #verify pivoting and arranging
->>>>>>> 08c1009dd8ade5471f263aa4ea02f4e3e14961bc
 
 FullData_left<- left_join(TPCData, EnviroData_wide) #merge EnviroData_wide into TPCData, omitting sites absent in EnviroData_wide
 #head(FullData_left) #demonstrate the merging
 
 FullData_left<- left_join(TPCData, EnviroData_wide) %>% #pipeline to relocate
   relocate(where(is.numeric), .after = where(is.character)) #move columns with strings to the left, numeric data to the right
-<<<<<<< HEAD
 #head(FullData_left)#demonstrate relocation
 
 ### Statistics ###
@@ -63,22 +50,10 @@ FullData_stats <- FullData_long %>% #create pipeline for stats calcs
   group_by (Variables, site.letter) %>% #select parameters to group by
   summarize(site_mean = mean(Values, na.rm = TRUE), #calc means in new column site_mean
             site_var = var(Values, na.rm = TRUE) #calc variances in new column site_var
-  #          ) %>%
-  #pivot_wider (names_from = "site.letter", "Variables",
-  #             values_from = "site_mean", "site_var"
-               )
+            #          ) %>%
+            #pivot_wider (names_from = "site.letter", "Variables",
+            #             values_from = "site_mean", "site_var"
+  )
 #view(FullData_stats)
 
 ### Tibble ###
-=======
-head(FullData_left)#demonstrate relocation
-
-### Statistics ###
-view(FullData_left) #open new tab with data to be processed
-FullData_long <- FullData_left %>% #create pipeline for pivoting
-  pivot_longer(cols = light:substrate.cover, #select all numeric data columns (light through substrate.cover)
-               names_to = "Variables", #create column for each variable type
-               values_to = "Values" #populate column withreadings for each data type
-  )
-view(FullData_long)
->>>>>>> 08c1009dd8ade5471f263aa4ea02f4e3e14961bc
