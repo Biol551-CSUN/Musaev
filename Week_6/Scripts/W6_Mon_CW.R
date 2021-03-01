d56026ab5d5f7ca410ec050235a92efd5987248a
kirill-biotech <krl_msv@hotmail.com>
2021-03-01 10:07:36 -0800

    RMarkdown Lab start

--- Week_6/Scripts/W6_Mon_CW.R
+++ Week_6/Scripts/W6_Mon_CW.R
@@ -1,2 +1,7 @@
 #install.packages("rmarkdown") #dynamic text formatting tool
 #install.packages("tinytex") #to export rmarkdown docs into pdf
+
+#install.packages("beepr") #extension to play sounds, for example when a job is done
+#library("beepr")
+
+
