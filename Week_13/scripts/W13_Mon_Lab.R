ce9bd5f699d09ff2322b170537fbebc6bb869f66
kirill-biotech <krl_msv@hotmail.com>
2021-04-26 11:24:31 -0700

    W13 Lab submission

--- Week_13/scripts/W13_Mon_Lab.R
+++ Week_13/scripts/W13_Mon_Lab.R
@@ -8,7 +8,7 @@ colnames(pools_data)<-c("orig_file", "mean_temp", "SD_temp", "mean_lux", "SD_lux
 ### Method 1: Loop ###
 
 for (i in 1:length(files)){ # for each file ...
-  pools_raw<-read.csv(paste0(PoolsPath,"/",files[i])) %>% # read it as csv from the pre-set path
+  pools_raw<-read_csv(paste0(PoolsPath,"/",files[i])) %>% # read it as csv from the pre-set path
     mutate(Intensity.lux = as.numeric(Intensity.lux)) # convert Intensity values to numeric (originally string)
   pools_data$orig_file[i]<-files[i] # populate column for original file
   pools_data$mean_temp[i]<-mean(pools_raw$Temp.C, na.rm =TRUE)
@@ -17,6 +17,25 @@ for (i in 1:length(files)){ # for each file ...
   pools_data$SD_lux[i]<-sd(pools_raw$Intensity.lux, na.rm =TRUE)
 } # do the 4 sets of calculation for mean and SD
 glimpse(pools_data) # check completion
+write_csv(pools_data, here("Week_13","output","W13_Mon_Lab_Loop.csv")) #write stats to file
 
 ### Method 2: map() ###
 
+# Clear environment because some function names are reused
+rm(list = ls(all.names = TRUE))
+# Reload original files and get full paths
+PoolsPath<-here("Week_13", "data", "homework") # set location with files to call on it when loading each file
+files <- dir(path = PoolsPath,pattern = ".csv", full.names = TRUE) # list all files in the folder containing .csv
+
+data<-files %>% 
+  set_names()%>% # set's the id of each list to the file name
+  map_df(read_csv,.id = "filename") %>% # map everything to a dataframe and put the id in a column called filename
+  mutate(Intensity.lux = as.numeric(Intensity.lux)) %>% # convert Intensity values to numeric (originally string)
+  group_by(filename) %>%
+  summarise(mean_temp = mean(Temp.C, na.rm = TRUE),
+            SD_temp = sd(Temp.C,na.rm = TRUE),
+            mean_lux = mean(Intensity.lux, na.rm = TRUE),
+            SD_lux = sd(Intensity.lux, na.rm = TRUE)
+  )
+#glimpse(data) # check output
+write_csv(data, here("Week_13","output","W13_Mon_Lab_Purrr.csv")) #write stats to file
